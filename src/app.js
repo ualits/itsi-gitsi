406c815e7409c564caf8dd06becde784db76b29b
Manel <manel.mena@ual.es>
2021-06-23 10:34:17 +0200

    evaluar repo up

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ var gh = new GitHub({
 var org = gh.getOrganization(process.env.ORG_NAME);
 
 var repoDef = {
-    "name": "prueba1",
+    "name": "runeterradra20",
     "private": true
 }
 
@@ -17,6 +17,7 @@ var args = process.argv;
 
 if(args[2]=="create") createRepo(repoDef)
 if(args[2]=="delete") deleteRepo(repoDef)
+if(args[2]=="repo") repoCount(repoDef)
 
 async function createRepo(repo){
     var resultado = await org.createRepo(repo, function(error,result,request){
@@ -28,10 +29,39 @@ async function createRepo(repo){
 
 async function deleteRepo(repo){
     
-   var repository = await gh.getRepo("ualdra","prueba1");
+   var repository = await gh.getRepo(process.env.ORG_NAME,repo.name);
 
    repository.deleteRepo((error,result,request)=>{
        console.log(result);
    })
 }
 
+async function repoCount(repo){
+
+    var issues = await gh.getIssues(process.env.ORG_NAME, repo.name)
+
+    var count = {};
+    await issues.listIssues({state:"closed"},(error,result,request)=>{
+        result.forEach(async element => {
+            var pointFactor=0;
+            await element.labels.forEach(label=>{
+                if(label.name=="1") pointFactor+=1;
+                if(label.name=="2") pointFactor+=2;
+                if(label.name=="3") pointFactor+=3;
+                if(label.name=="4") pointFactor+=4;
+                if(label.name=="5") pointFactor+=5;
+                if(label.name=="6") pointFactor+=6;
+                if(label.name=="7") pointFactor+=7;
+                if(label.name=="8") pointFactor+=8;
+                if(label.name=="9") pointFactor+=9;
+                if(label.name=="10") pointFactor+=10;
+            })
+            if(pointFactor==0) pointFactor=1;
+            await element.assignees.forEach(assignee=>{
+                if(!count[assignee["login"]]) count[assignee["login"]]=pointFactor
+                else count[assignee["login"]]+=pointFactor
+            })
+        });
+    })
+    console.log(count)
+}
